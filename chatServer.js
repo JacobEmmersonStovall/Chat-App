3fb49348eb21b6b399dc7267384e7389ff6919f0
Jacob Stovall <jacobestovall@gmail.com>
2015-05-30 19:22:47 -0700

    Added Functionality
    Created Username System, More ChatBot Functionality, and Automatic
    scrolling.

--- chatServer.js
+++ chatServer.js
@@ -6,23 +6,80 @@ var app = express();
 var server = http.Server(app);
 var io = require('socket.io')(http).listen(server);
 
+var userCount = 0
+var usernames = [];
+
 app.use(express.static(__dirname + '/assets'));
 
 app.get('/',function(req,res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var chatBot = function(socket,msg){
+  switch(msg){
+    case "!helloBot":
+      return "Chat Bot: Hello "+socket.username;
+    case "!numUsers":
+      if(userCount == 1){
+        return "Chat Bot: There is "+userCount+" user currently on.";
+      }
+      else {
+        return "Chat Bot: There are "+userCount+" users currently on.";
+      }
+    case "!users":
+      if(usernames.length == 1){
+        return "You're the only person on right now."
+      }
+      var userString = "Chat Bot: The people currently on are "
+      for(i = 0; i < usernames.length; i++){
+        if(i == usernames.length-1){
+          userString = userString + "and "+usernames[i];
+        }
+        else{
+          userString = userString + usernames[i] + ", ";
+        }
+      }
+      return userString;
+    default:
+      return "Chat Bot: I'm sorry I don't know what you want.";
+  }
+}
+
+var outputMessage = function(usr,msg){
+  return usr+ ": "+msg;
+}
+
 io.on('connection', function (socket) {
         console.log('User Connected.');
-        socket.on('chat message', function (msg) {
-          if(msg === '!helloBot'){
-            io.to(socket.id).emit('chat message', msg);
-            io.to(socket.id).emit('chat message', "Chat Bot: Hello User");
+        userCount = userCount + 1;
+        var chatBotHello = "Chat Bot: <em>Hello User, I'm Chat Bot. In" +
+         " addition to chatting with friends, you can talk to me and I'll give"+
+         " you different types of info. (Don't worry, if you type the command"+
+         " write, no one will see us talking.) To see all my commands, just type !helloBot";
+        io.to(socket.id).emit('chat message', chatBotHello);
+        socket.on('username', function(name){
+          var found = usernames.indexOf(name) > -1;
+          if(found){
+            io.to(socket.id).emit('newName');
           }
           else{
-            io.emit('chat message', msg);
+            socket.username = name;
+            usernames.push(name);
           }
-    });
+        });
+        socket.on('chat message', function (msg) {
+          if(msg.charAt(0) == '!'){
+            io.to(socket.id).emit('chat message', outputMessage(socket.username,msg));
+            io.to(socket.id).emit('chat message', chatBot(socket,msg));
+          }
+          else{
+            io.emit('chat message', outputMessage(socket.username,msg));
+          }
+        });
+        socket.on('disconnect', function(){
+          userCount = userCount - 1;
+          usernames.splice(usernames.indexOf(socket.username),1);
+        });
 });
 
 server.listen(3000, function(){
